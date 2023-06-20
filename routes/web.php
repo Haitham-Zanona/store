53e64a60e3b1c10b63ab42a2353f0146ffc3b4e9
Haitham-Zanona <hythamzanona@gmail.com>
2023-06-20 23:57:25 +0300

    21/6

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', [DashboardController::class, 'index']);
+
+//at Laravel -v  7 or less
+// Route::get('/dashboard', 'App\Http\Controllers\DashboardController@index');
