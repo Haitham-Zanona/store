c917ade32a1e0ff6ba61bdb763bdeab67be38fca
Haitham-Zanona <hythamzanona@gmail.com>
2023-07-05 17:13:02 +0300

    auth

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
+
+    public function __construct(){
+        //$this->middleware(['auth','verified']); #Authenticates user and verified email address before
+    }
     //Actions
     public function index() {
+        $title = 'Store';
+        $user = Auth::user();
         //Return response: view, json, redirect, file
-        return view('dash.index');
+        return view('dashboard.index', [
+            "title" => $title,
+            "user"  => $user
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,14 +19,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dash/index', [DashboardController::class, 'index']);
 
 //at Laravel -v  7 or less
 // Route::get('/dashboard', 'App\Http\Controllers\DashboardController@index');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index'])
+        ->middleware(['auth', 'verified'])
+        ->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -34,3 +34,4 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
