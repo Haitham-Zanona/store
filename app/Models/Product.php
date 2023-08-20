6eca06b292175bbc484abca0311d9c43c5ca16b1
Haitham-Zanona <hythamzanona@gmail.com>
2023-08-21 02:30:29 +0300

    front

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,15 +5,17 @@ namespace App\Models;
 use App\Models\Tag;
 use App\Models\Store;
 use App\Models\Category;
+use Illuminate\Support\Str;
 use App\Models\Scopes\StoreScope;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
     use HasFactory;
 
-    protected $fillable= [
+    protected $fillable = [
         'name',
         'description',
         'image',
@@ -33,14 +35,17 @@ class Product extends Model
         static::addGlobalScope('store', new StoreScope());
     }
 
-    public function category(){
+    public function category()
+    {
         return $this->belongsTo(Category::class, 'category_id', 'id');
     }
-    public function store(){
+    public function store()
+    {
         return $this->belongsTo(Store::class, 'store_id', 'id');
     }
 
-    public function tags(){
+    public function tags()
+    {
         return $this->belongsToMany(
             Tag::class,     //Related Model
             'product_tag',  //Pivot table name
@@ -51,7 +56,28 @@ class Product extends Model
         );
     }
 
+    public function scopeActive(Builder $builder)
+    {
+        $builder->where('status', '=', 'active');
+    }
 
+    //Accessors
+    public function getImageUrlAttribute()
+    {
+        if ($this->image) {
+            return 'https://www.ehabra.com/storage/images/documents/_res/wrh/def_product.png';
+        }
+        if (Str::startsWith($this->image, ['http://', 'https://'])) {
+            return $this->image;
+        }
+        return asset('storage/' .$this->image);
+    }
 
+    public function getSalePercentAttribute(){
+        if (!$this->compare_price){
+            return 0;
+        }
+        return round((1 - (($this->price /$this->compare_price)*100)) *-1 ,1).'%';
+    }
     public $timestamps = false;
 }
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\front\HomeController;
+use App\Http\Controllers\front\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,9 +17,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])
+    ->name('home');
+
+Route::get('/products', [ProductController::class, 'index'])
+    ->name('products.index');
+
+Route::get('/products/{product:slug}', [ProductController::class, 'show'])
+    ->name('product.show');
 
 Route::get('/test', function() {
     return view('test');
