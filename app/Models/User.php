1fe617b6da5df86795bed0f089e3c22bfc2e32c3
Haitham-Zanona <hythamzanona@gmail.com>
2023-09-14 01:33:31 +0300

    two-factor auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,10 +9,11 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, TwoFactorAuthenticatable;
 
     /**
      * The attributes that are mass assignable.
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Front\Auth\TwoFactorAuthenticationController;
 use App\Http\Controllers\Front\CartController;
 use App\Http\Controllers\front\HomeController;
 use App\Http\Controllers\front\ProductController;
@@ -32,6 +33,9 @@ Route::resource('cart', CartController::class);
 
 Route::get('checkout', [CheckoutController::class, 'create'])->name('checkout');
 Route::post('checkout', [CheckoutController::class, 'store']);
+
+Route::get('auth/user/2fa', [TwoFactorAuthenticationController::class, 'index'])
+    ->name('front.2fa');
 
 Route::get('/test', function() {
     return view('test');
